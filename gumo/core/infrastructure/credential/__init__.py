a8b33a7c726d46947a5ff5dc9ba6021d2dc38097
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-05-02 18:44:28 +0900

    Support IDTokenCredential builder

--- gumo/core/infrastructure/credential/__init__.py
+++ gumo/core/infrastructure/credential/__init__.py
@@ -1,10 +1,13 @@
 import os
 import json
 
-from google.oauth2 import credentials
+from typing import Optional
+from typing import Union
+from typing import Tuple
+
+import google.auth.transport.requests
 from google.oauth2 import service_account
 from google.auth import compute_engine
-
 from google.cloud import storage
 from injector import inject
 
@@ -12,18 +15,14 @@ from gumo.core.exceptions import ServiceAccountConfigurationError
 from gumo.core.injector import injector
 from gumo.core.domain.configuration import GumoConfiguration
 
+DEFAULT_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+
+Credentials = Union[service_account.Credentials, compute_engine.Credentials]
+IDTokenCredentials = Union[service_account.IDTokenCredentials, compute_engine.IDTokenCredentials]
+Request = google.auth.transport.Request
+
 
 class GoogleOAuthCredentialManager:
-    _credential = None
-
-    @classmethod
-    def get_credential(cls) -> credentials.Credentials:
-        if cls._credential:
-            return cls._credential
-
-        cls._credential = injector.get(cls).build_credential()
-        return cls._credential
-
     @inject
     def __init__(
             self,
@@ -32,7 +31,7 @@ class GoogleOAuthCredentialManager:
         self._gumo_configuration = gumo_configuration
         self._credential_config = self._gumo_configuration.service_account_credential_config
 
-    def build_credential(self) -> credentials.Credentials:
+    def build_credential(self) -> Credentials:
         _credentials = None
 
         try:
@@ -57,6 +56,44 @@ class GoogleOAuthCredentialManager:
 
         return _credentials
 
+    def build_request(self) -> Request:
+        return google.auth.transport.requests.Request()
+
+    def build_id_token_credential(
+            self,
+            target_audience: str,
+            with_refresh: bool = True,
+            token_uri: Optional[str] = None,
+    ) -> Tuple[IDTokenCredentials, Request]:
+        request = self.build_request()
+        _id_token_credential = None
+
+        try:
+            if self._gumo_configuration.is_google_app_engine:
+                _id_token_credential = compute_engine.IDTokenCredentials(
+                    request=request,
+                    target_audience=target_audience,
+                )
+                if with_refresh:
+                    _id_token_credential.refresh(request=request)
+            elif 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
+                _credential = self.build_credential()
+                _id_token_credential = service_account.IDTokenCredentials(
+                    signer=_credential.signer,
+                    service_account_email=_credential.service_account_email,
+                    token_uri=token_uri if token_uri else DEFAULT_TOKEN_URI,
+                    target_audience=target_audience,
+                )
+        except ServiceAccountConfigurationError:
+            raise
+        except RuntimeError as e:
+            raise ServiceAccountConfigurationError(e)
+
+        if _id_token_credential is None:
+            raise ServiceAccountConfigurationError(f'ServiceAccount Credential Config disabled.')
+
+        return (_id_token_credential, request)
+
     def _get_content_from_storage(self):
         storage_client = storage.Client()
         bucket = storage_client.get_bucket(bucket_name=self._credential_config.bucket_name)
@@ -77,5 +114,9 @@ class GoogleOAuthCredentialManager:
         return json.loads(content)
 
 
-def get_google_oauth_credential() -> credentials.Credentials:
-    return GoogleOAuthCredentialManager.get_credential()
+def get_google_oauth_credential() -> Credentials:
+    return injector.get(GoogleOAuthCredentialManager).build_credential()
+
+
+def get_google_id_token_credential() -> Tuple[IDTokenCredentials, Request]:
+    return injector.get(GoogleOAuthCredentialManager).build_id_token_credential()
